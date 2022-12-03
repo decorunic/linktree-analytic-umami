dc00329d606b8f63dd5fb0ff58967c983d10dea9
virgiawankusuma <virgiawan150@gmail.com>
2022-12-03 15:40:28 +0700

    add backbutton and change name

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -24,14 +24,20 @@ export default function Header() {
     <>
       {allowUpdate && <UpdateNotice />}
       <header className={classNames(styles.header, 'row')}>
-        <div className={styles.title}>
-          <Link href="https://decorunic.id/linktree/admin">
-            <div class="back-button">
+        <div 
+          className={styles.title}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+          >
+          <div class="back-button">
+            <Link href="https://decorunic.id/linktree/admin">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
               </svg>
-            </div>
-          </Link>
+            </Link>
+          </div>
           <Link href={isSharePage ? HOMEPAGE_URL : '/'}>Analytics</Link>
         </div>
         <HamburgerButton />
